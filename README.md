a851385467f67a8981479439bdc4f4081850be74
Caroline Patenaude <56038580+CRLNP@users.noreply.github.com>
2024-11-21 12:04:43 -0500

    mise en ligne initiale
    Test de mise en ligne initiale

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Tiohtiake
-Bibliographie du projet Tiohtià:ke
+
+
+Code adapté de:
+[Repository for the book Among Digitized Manuscripts by L.W. Cornelis van Lit](https://github.com/among/digitized-manuscripts)
+
+ 
---
+++ textsInterface.json.js
@@ -0,0 +1,165 @@
+var texts =
+    {
+        "headline":
+            {
+                "english": "Tiohtià:ke Project",
+                "german": "Projet Tiohtià:ke"
+            }
+        ,
+        "stiftFlorianTip":
+            {
+                "english": "Contact - Université de Montréal",
+                "german": "Contact - Université de Montréal"
+            }
+        ,
+        "digitalOrientalistTip":
+            {
+                "english": "Christian Gates St-Pierre",
+                "german": "Christian Gates St-Pierre"
+            }
+        ,
+        "flagTip":
+            {
+                "english": "Anglais",
+                "german": "Francais"
+            }
+        ,
+        "welcomeHeader":
+            {
+                "english": "Projet Tiohtià:ke",
+                "german": "Tiohtià:ke Project"
+            }
+        ,
+        "welcomeBody":
+            {
+                "english": "Reports and publications produced as part of the Tiohtià:ke project, a partnership between the Pointe-à-Callière Museum, the Université de Montréal and the Mohawk Council of Kahnawà:ke",
+                "german": "Rapports et publications produits dans le cadre du projet Tiohtià:ke, partenariat entre le Musée Pointe-à-Callière, l’Université de Montréal et le Conseil Mohawk de Kahnawà:ke"
+            }
+        ,
+        "searchButton":
+            {
+                "english": "Search",
+                "german": "Chercher"
+            }
+        ,
+        "searchBox":
+            {
+                "english": "Search...",
+                "german": "Rechercher..."
+            }
+        ,
+        "searchTip":
+            {
+                "english": "Insert a term and clic Search",
+                "german": "Inscrire un terme et cliquer Chercher"
+            }
+        ,
+        "sortBy":
+            {
+                "english": "Sort by: ",
+                "german": "Trier par: "
+            }
+        ,
+        "sortTitleCaption":
+            {
+                "english": "Title",
+                "german": "Titre"
+            }
+        ,
+        "sortAuthorCaption":
+            {
+                "english": "Author(s)",
+                "german": "Auteur.e.s"
+            }
+        ,
+        "sortYearCaption":
+            {
+                "english": "Year",
+                "german": "Année"
+            }
+        ,
+        "beginTitleCatalog":
+            {
+                "english": "Catalog",
+                "german": "Catalogue"
+            }
+        ,
+        "noResultsTitleCatalog":
+            {
+                "english": "No results, showing all entries instead:",
+                "german": "Pas de résultats, présentation de toutes les entrées:"
+            }
+        ,
+        "againTitleCatalog":
+            {
+                "english": "Showing all entries again:",
+                "german": "Présentation de toutes les entrées:"
+            }
+        ,
+        "foundItemsCatalog":
+            {
+                "english": "Number of results:",
+                "german": " Nombre de résultats:"
+            },
+        "authorCatalog":
+            {
+                "english": "Author(s): ",
+                "german": "Auteur.e.s: "
+            }
+        ,
+        "editorCatalog":
+            {
+                "english": "Editor: ",
+                "german": "Éditeur: "
+            },
+        "translatorCatalog":
+            {
+                "english": "Translator: ",
+                "german": "Traducteur: "
+            },
+        "noAuthorCatalog":
+            {
+                "english": "No author details. ",
+                "german": "Aucun détail d'auteur. "
+            },
+        "numberVolumesCatalog":
+            {
+                "english": "Number of volumes: ",
+                "german": "Nombre de volumes: "
+            },
+        "publisherCatalog":
+            {
+                "english": "Publisher: ",
+                "german": "Éditeur: "
+            },
+        "placeCatalog":
+            {
+                "english": "Place: ",
+                "german": "Lieu: "
+            },
+        "yearCatalog":
+            {
+                "english": "Year: ",
+                "german": "Année: "
+            },
+        "additionalCatalog":
+            {
+                "english": "Abstract: ",
+                "german": "Résumé: "
+            },
+        "callNumberCatalog":
+            {
+                "english": "Callnumber: ",
+                "german": "Cote: "
+            },
+        "urlCatalog":
+            {
+                "english": "Link to resource",
+                "german": "Lien vers la ressource"
+            },
+        "closeCatalog":
+            {
+                "english": "Close",
+                "german": "Fermer"
+            }
+    }
